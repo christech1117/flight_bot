36a2400be07b6a3fee7e1cd082ae9f11cdb1a9ea
artopll <artopllting@gmail.com>
2018-05-21 11:32:33 +0800

    import PostBackEvent

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage,TemplateSendMessage,ButtonsTemplate,PostbackTemplateAction
+    MessageEvent, TextMessage, TextSendMessage, ImageSendMessage,TemplateSendMessage,ButtonsTemplate,PostbackTemplateAction,PostbackEvent
 )
 import os
 import sys
