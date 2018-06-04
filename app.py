dcdfe60545ad828426686567f46bc948f5ec2e5e
EricJhang <housekeepbao@gmail.com>
2018-06-04 15:10:58 +0800

    fixed datetime type error

--- app.py
+++ app.py
@@ -199,6 +199,10 @@ def choice_datatime(type):
     now_time = datetime.datetime.now().strftime("%Y-%m-%d ")
     max_time_tmp = datetime.datetime.now()+datetime.timedelta(days=365)
     max_time = max_time_tmp.strftime("%Y-%m-%d ")
+    print(now_time)
+    print(type(now_time))
+    print(max_time)
+    print(type(max_time))
     if(type == type_of_return):
         title_string =" 請選擇回國日期"
         text_string = "選擇日期"
