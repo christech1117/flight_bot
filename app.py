b3713d12e9ff1bf4e6e34d51e32dc6c87b68d848
EricJhang <housekeepbao@gmail.com>
2018-06-08 14:45:31 +0800

    test ads photo

--- app.py
+++ app.py
@@ -110,6 +110,8 @@ def handle_message(event):
         message_text_tmp = "請問出發地點 ?  (例如:台北、TPE)"
         message = TextSendMessage(text=message_text_tmp)        
         replay_message(event,message)
+    elif("廣告" == event.message.text):
+        push_ads(user_key)
     elif (user_key not in session_dict) :
         if(('搜尋機票' in event.message.text) or ('查詢機票' in event.message.text)):
             session_second_list = []
@@ -280,13 +282,15 @@ def search_air_tickest(event):
     )
     push_message(user_id,buttons_template_message)
 def push_ads(user_id):
+    photo_url = 'https://lh3.googleusercontent.com/qOTCGkpe1-_pSkloQHJAGlHAygcKY7sd7e8DRm1FJfEZPkzXc0Isk47QCy5nOA3lvZ6adGwcqxUXIjY2lxfM=w1929-h888-rw'
+    link_url = 'https://www.google.com.tw/maps/place/%E5%8F%A4%E6%97%A9%E5%91%B3%E5%B0%8F%E5%90%83%E5%BA%97/@25.0629705,121.5012555,23.8z/data=!4m8!1m2!2m1!1z5Y-w5YyX5qmLIOe-jumjnw!3m4!1s0x3442a92298613293:0xcff4aac1356b306!8m2!3d25.0629585!4d121.50107?hl=zh-TW'
     imagemap_message = ImagemapSendMessage(
-        base_url='https://example.com/base',
+        base_url=photo_url,
         alt_text='this is an imagemap',
         base_size=BaseSize(height=1040, width=1040),
         actions=[
             URIImagemapAction(
-                link_uri='https://example.com/',
+                link_uri=link_url,
                 area=ImagemapArea(
                     x=0, y=0, width=520, height=1040
                 )
