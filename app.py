cc04a5132674db436a94b08955ff1be84ff75074
Jackal Kao <shinjikao@gmail.com>
2018-06-20 02:37:55 +0800

    change db name to line_member

--- app.py
+++ app.py
@@ -317,7 +317,7 @@ def save_memberInfo_data(user_id, phoneNumber, email, gender):
     # Note that the insert method can take either an array or a single dict.
     #json_str = json.dumps(user.__dict__, ensure_ascii=False).encode('utf8')
     #data = {'user': user.__dict__}
-    session_collection = db['member']
+    session_collection = db['line_member']
     inserted_id = session_collection.insert_one(user.__dict__).inserted_id
     if inserted_id:
         return True
