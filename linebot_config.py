fccc6236104516b53a515e5b97c63a51ad6d6473
JHANG JHIH KAI <eric_jhang@flightgoai.com>
2018-06-20 18:07:49 +0800

    add config

--- linebot_config.py
+++ linebot_config.py
@@ -5,6 +5,12 @@ from linebot import (
 import os
 
 class linebotConfig():
+    '''config 使用說明
+        可自己git clone到本機端之後，使用Ngrok
+        並將自己設定的Dev Line bot  token 跟  channel screct 填到
+        self.token = token
+        self.screct = channel screct
+        '''
     def __init__(self):
         self.token = os.environ['linetoken']
         self.screct = os.environ['linechannel']
