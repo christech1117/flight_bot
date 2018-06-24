577edf65629da743910b8c108b38b85770064f11
JHANG JHIH KAI <eric_jhang@flightgoai.com>
2018-06-24 15:02:57 +0800

    implement ifirstlogin

--- save_db.py
+++ save_db.py
@@ -1,15 +1,25 @@
 
 from models.User import LineUser
 import pymongo
-
+import requests
 uri = 'mongodb://heroku_g4mqlp4n:b2fuh42r8dvlnaofkcrv97sv93@ds225010.mlab.com:25010/heroku_g4mqlp4n'
 client = pymongo.MongoClient(uri)
 db = client.get_default_database()
 PROVIDER = 'LINE'
 VENDOR = ['雄獅', '可樂', '山富']
-def is_first_Login(event):
+
+def get_html(url):
+    response = requests.request("GET", url)
+    return response
+def is_first_Login(user_key):
     # 先把method開出來，到時候去搜尋FlightGo會員資料庫，確認會員是不是第一次使用
-    isfirst = True
+    url = "https://flightgo-dashboard.herokuapp.com/api/lineuser/"+user_key
+
+    response = get_html(url)
+    if(response.status_code == 200):
+        isfirst = False
+    else:
+        isfirst = True
     return isfirst
 def save_memberInfo_data(user_id, phoneNumber, email, gender,profile):
     # 將會員資料傳到後端，讓後端進行儲存
