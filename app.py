4d597a36c1db5aa6df17d632369c582620e38a7f
JHANG JHIH KAI <eric_jhang@flightgoai.com>
2018-07-03 11:54:24 +0800

    try to test Flex template

--- app.py
+++ app.py
@@ -27,7 +27,17 @@ from linebot.models import (
     StickerSendMessage,
     CarouselTemplate,
     CarouselColumn,
-    ConfirmTemplate)
+    ConfirmTemplate,
+    BubbleContainer,
+    BoxComponent,
+    TextComponent,
+    URIAction,
+    IconComponent,
+    ImageComponent,
+    SeparatorComponent,
+    ButtonComponent,
+    FlexSendMessage,
+    SpacerComponent)
 import os
 import sys
 import pymongo
@@ -234,7 +244,9 @@ def handle_message(event):
     elif user_key in session_dict and len(session_dict[user_key]) >= 1:
         search_air_info_session(event)
     elif "客服問卷" in event.message.text:
-        customer_questionnaire(user_key )
+        customer_questionnaire(user_key)
+    elif "Flex" in event.message.text:
+        line_flex_example(user_key)
     else:
         other_session(event)
 
@@ -560,6 +572,7 @@ def share_link_info(user_id):
     push_message(user_id, link_url_json)
 
 
+
 def ask_paper_memberInfo(event):
     print('#ask_paper_memberInfo1')
     #global ask_member_Info_session_dict
@@ -716,6 +729,7 @@ def customer_questionnaire(user_key):
     push_tickets_info = TextSendMessage(text=tickets_text)
     push_message(user_key, [push_tickets_info,buttons_template,buttons_template_second])
 
+
 def ask_user_favorite_travel(user_key):
     if user_key not in ask_user_favorite_session_dict:
         ask_user_favorite_session_dict[user_key] = ["ask_session_start"]
@@ -828,6 +842,109 @@ def ask_user_favorite_travel(user_key):
             alt_text='喜好旅遊類型',
             template=CarouselTemplate(columns=columns_list_2))
         push_message(user_key, carousel_template_message2)
+
+def line_flex_example(user_key):
+    bubble = BubbleContainer(
+        direction='ltr',
+        hero=ImageComponent(
+            url='https://example.com/cafe.jpg',
+            size='full',
+            aspect_ratio='20:13',
+            aspect_mode='cover',
+            action=URIAction(uri='http://example.com', label='label')
+        ),
+        body=BoxComponent(
+            layout='vertical',
+            contents=[
+                # title
+                TextComponent(text='Brown Cafe', weight='bold', size='xl'),
+                # review
+                BoxComponent(
+                    layout='baseline',
+                    margin='md',
+                    contents=[
+                        IconComponent(size='sm', url='https://example.com/gold_star.png'),
+                        IconComponent(size='sm', url='https://example.com/grey_star.png'),
+                        IconComponent(size='sm', url='https://example.com/gold_star.png'),
+                        IconComponent(size='sm', url='https://example.com/gold_star.png'),
+                        IconComponent(size='sm', url='https://example.com/grey_star.png'),
+                        TextComponent(text='4.0', size='sm', color='#999999', margin='md',
+                                      flex=0)
+                    ]
+                ),
+                # info
+                BoxComponent(
+                    layout='vertical',
+                    margin='lg',
+                    spacing='sm',
+                    contents=[
+                        BoxComponent(
+                            layout='baseline',
+                            spacing='sm',
+                            contents=[
+                                TextComponent(
+                                    text='Place',
+                                    color='#aaaaaa',
+                                    size='sm',
+                                    flex=1
+                                ),
+                                TextComponent(
+                                    text='Shinjuku, Tokyo',
+                                    wrap=True,
+                                    color='#666666',
+                                    size='sm',
+                                    flex=5
+                                )
+                            ],
+                        ),
+                        BoxComponent(
+                            layout='baseline',
+                            spacing='sm',
+                            contents=[
+                                TextComponent(
+                                    text='Time',
+                                    color='#aaaaaa',
+                                    size='sm',
+                                    flex=1
+                                ),
+                                TextComponent(
+                                    text="10:00 - 23:00",
+                                    wrap=True,
+                                    color='#666666',
+                                    size='sm',
+                                    flex=5,
+                                ),
+                            ],
+                        ),
+                    ],
+                )
+            ],
+        ),
+        footer=BoxComponent(
+            layout='vertical',
+            spacing='sm',
+            contents=[
+                # callAction, separator, websiteAction
+                SpacerComponent(size='sm'),
+                # callAction
+                ButtonComponent(
+                    style='link',
+                    height='sm',
+                    action=URIAction(label='CALL', uri='tel:000000'),
+                ),
+                # separator
+                SeparatorComponent(),
+                # websiteAction
+                ButtonComponent(
+                    style='link',
+                    height='sm',
+                    action=URIAction(label='WEBSITE', uri="https://example.com")
+                )
+            ]
+        ),
+    )
+    message = FlexSendMessage(alt_text="hello", contents=bubble)
+    push_message(user_key, message)
 
 
 if __name__ == "__main__":
